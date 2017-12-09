e04b1440c390c87e14a009e4da3592d6ed39f2cf
Dušan Reljić <reljicd@gmail.com>
2017-12-09 15:34:23 +0100

    registering Post and Comment models in admin configuration

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.comment import Comment
+from .models.post import Post
+
+admin.site.register(Post)
+admin.site.register(Comment)
