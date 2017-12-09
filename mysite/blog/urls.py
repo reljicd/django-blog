cf4b9894ff77bd18c6119897379f7355934c72a3
Dušan Reljić <reljicd@gmail.com>
2017-12-09 15:32:32 +0100

    added blog app urlpatterns

---
+++ mysite/blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+
+from .views.comment import CommentCreate
+from .views.home import home
+from .views.post import *
+
+app_name = 'blog'
+urlpatterns = [
+    # ex: /blog/
+    path('', home, name='home'),
+    # ex: /blog/dusan
+    path('<str:username>', home, name='user_posts'),
+    # ex: /blog/post/5/
+    path('post/<int:pk>/', PostView.as_view(), name='post'),
+    # ex: /blog/post/create/
+    path('post/create/', PostCreate.as_view(), name='create_post'),
+    # ex: /blog/post/5/update/
+    path('post/create/<int:pk>/update', PostUpdate.as_view(), name='update_post'),
+    # ex: /blog/post/5/delete/
+    path('post/<int:pk>/delete/', PostDelete.as_view(), name='delete_post'),
+    # ex: /blog/post/5/comment/
+    path('post/<int:pk>/comment/', CommentCreate.as_view(), name='create_comment')
+]
