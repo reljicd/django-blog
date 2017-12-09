6faed448e4112cb44be7824a17f9fd41c5fccf80
Dušan Reljić <reljicd@gmail.com>
2017-12-09 15:20:10 +0100

    adding BlogConfig to INSTALLED_APPS, and '/blog' as ACCOUNT_LOGOUT_REDIRECT_URL

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ACCOUNT_LOGOUT_REDIRECT_URL = "/blog"
