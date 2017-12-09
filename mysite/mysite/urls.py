60325384ce24373c7e162740a9e9db2f52413669
Dušan Reljić <reljicd@gmail.com>
2017-12-09 15:31:55 +0100

    added base urlpatterns

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+
+from blog.views.register import RegisterView
 
 urlpatterns = [
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html')),
+    path('logout/', auth_views.LogoutView.as_view(next_page='blog:home')),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('', include('django.contrib.auth.urls'))
 ]
