837ae14d320dd207176064b258375ce7fa20e2fc
Dušan Reljić <reljicd@gmail.com>
2017-12-16 17:08:55 +0100

    implemented tests for home view

---
+++ blog/tests/views/test_home.py
@@ -0,0 +1,58 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+from blog.models.post import Post
+from blog.views.home import NUM_OF_POSTS
+
+
+class HomeViewTest(TestCase):
+    USERNAME = 'john_lennon'
+    PASSWORD = 'john_password'
+
+    fixtures = ['users', 'posts', 'comments']
+
+    def setUp(self):
+        self.test_user = User.objects.create_user(self.USERNAME, 'lennon@thebeatles.com', self.PASSWORD)
+        self.test_user.first_name = 'John'
+        self.test_user.last_name = 'Lennon'
+        self.test_user.save()
+        Post.objects.create(user=self.test_user, title='Title 1', body='Lorem ipsum')
+        Post.objects.create(user=self.test_user, title='Title 2', body='Lorem ipsum')
+        Post.objects.create(user=self.test_user, title='Title 3', body='Lorem ipsum')
+
+    def test_home_page_all_posts(self):
+        url = reverse('blog:home')
+        response = self.client.get(url)
+        posts = [post.__repr__() for post in Post.objects.all().order_by('-pub_date')[:NUM_OF_POSTS]]
+        self.assertQuerysetEqual(response.context['posts'], posts)
+
+    def test_home_page_non_logged_user_buttons(self):
+        url = reverse('blog:home')
+        response = self.client.get(url)
+        self.assertContains(response, 'Registration')
+        self.assertContains(response, 'Login')
+        self.assertNotContains(response, 'New Post')
+        self.assertNotContains(response, 'Sign Out')
+
+    def test_home_page_logged_user_buttons(self):
+        self.client.login(username=self.USERNAME, password=self.PASSWORD)
+        url = reverse('blog:home')
+        response = self.client.get(url)
+        self.assertNotContains(response, 'Registration')
+        self.assertNotContains(response, 'Login')
+        self.assertContains(response, 'New Post')
+        self.assertContains(response, 'Sign Out')
+
+    def test_user_posts_page_only_users_posts(self):
+        # /blog/john_lennon
+        url = reverse('blog:user_posts', kwargs={'username': self.test_user.username})
+        response = self.client.get(url)
+        posts = [post.__repr__()
+                 for post
+                 in Post.objects.filter(user=self.test_user).order_by('-pub_date')[:NUM_OF_POSTS]]
+        self.assertQuerysetEqual(response.context['posts'], posts)
+        self.assertContains(response, self.test_user.first_name)
+        self.assertContains(response, self.test_user.last_name)
+        self.assertEqual(response.context['first_name'], self.test_user.first_name)
+        self.assertEqual(response.context['last_name'], self.test_user.last_name)
